4217ede62eb591d764104f21e890a0449fbe9eba
reemmalyamani <alyamanireem@gmail.com>
2025-12-11 07:26:39 +0000

    fising betty style for task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,20 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
  *
- * Return: void
+ * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
 
-    /* Delete children first */
     binary_tree_delete(tree->left);
     binary_tree_delete(tree->right);
-
-    /* Then delete this node */
     free(tree);
 }
